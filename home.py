e5f3a51e3e036529fdaebf5e23253a416bf77998
pradeepkarthik77 <77573751+pradeepkarthik77@users.noreply.github.com>
2023-06-06 14:55:32 +0530

    added weekly prediction

--- home.py
+++ home.py
@@ -249,22 +249,18 @@ class Solution:
             widget_container2 = st.empty()
             widget_container2.metric(label="Predicted Electricity Bill", value=charges, delta="Rupees")
         
-        weeklydata = model.make_future_dataframe(periods=28, freq='D')  # Predicting 30 days from the last date in the dataset
+        
+        week1 = round(forecast['yhat'].iloc[-7:].sum(),2)
+        week2 = round(forecast['yhat'].iloc[-14:-7].sum(),2)
+        week3 = round(forecast['yhat'].iloc[-21:-14].sum(),2)
+        week4 = round(forecast['yhat'].iloc[-28:-21].sum(),2)
 
-        week1 = weeklydata['yhat'].iloc[-7:]
-        # week2 = weeklydata['yhat'].iloc[-14:-7].sum()
-        # week3 = weeklydata['yhat'].iloc[-21:-14].sum()
-        # week4 = weeklydata['yhat'].iloc[-28:-21].sum()
-
-        st.write(week1)
-
-        st.write("Weekly Prediction for the next month")
+        st.header("Weekly Prediction for the next month")
 
         st.metric(label="Week 1",value = week1,delta="watts")
-        # st.metric(label="Week 2",value = week2,delta="watts")
-        # st.metric(label="Week 3",value = week3,delta="watts")
-        # st.metric(label="Week 4",value = week4,delta="watts")
-
+        st.metric(label="Week 2",value = week2,delta="watts")
+        st.metric(label="Week 3",value = week3,delta="watts")
+        st.metric(label="Week 4",value = week4,delta="watts")
 
 
     def control_device(self):
